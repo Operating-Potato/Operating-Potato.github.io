a7565b3a04676124cb2f0e44ea925d44009cdad3
Operating-Potato <c04511234@gmail.com>
2025-09-02 18:38:40 -0600

    Added automatic sidebar dark mode, settings page

---
+++ Settings/scripts.js
@@ -0,0 +1,11 @@
+const root = document.documentElement;
+
+let darkSidebar = window.getComputedStyle(root);
+
+let propertyValue = darkSidebar.getPropertyValue('sidebar-colour');
+
+function toggleDark() {
+    if 
+}
+
+                    root.style.setProperty('--sidebar-colour', 'rgba(0,0,0,0.5)');
